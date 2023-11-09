eab95e9fc470a9e5d5f3d36c2a6ce45f30554011
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2023-11-10 05:52:25 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <h3 className="font-bold lg:text-4xl text-2xl lg:w-full w-4/5 lg:mb-12 mb-8">Front-End Website Developer</h3>
         <p className="font-light lg:text-lg text-base lg:w-4/5 w-full lg:leading-normal leading-8">
           Hello Folks! My name <span className="border-b-2 border-black text-lg leading-tight inline font-medium">Febrian Daffa Eka Putra</span>, I'm a{" "}
-          <span className="border-b-2 border-black text-lg leading-tight inline font-medium">Front-End Developer</span> with expertise in{" "}
+          <span className="border-b-2 border-black text-lg leading-tight inline font-medium">Front-End Website Developer</span> with expertise in{" "}
           <span className="border-b-2 border-black text-lg leading-tight inline font-medium">HTML, CSS, and JavaScript</span>. Currently, I'm expanding my skills to become a proficient in{" "}
           <span className="border-b-2 border-black text-lg leading-tight inline font-medium">Front-End Mobile Developer</span>. Let's connect and work together!
         </p>
